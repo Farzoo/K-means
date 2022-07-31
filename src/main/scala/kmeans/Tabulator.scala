01d9fc61c3799366845d4e815e63eda605994e9a
Diogo Ribeiro <diogo95400ribeiro@gmail.com>
2022-07-31 18:36:53 +0100

    Version finale K-means + objet tabulator

---
+++ src/main/scala/kmeans/Tabulator.scala
@@ -0,0 +1,42 @@
+package kmeans
+
+object Tabulator {
+  def format(table: Seq[Seq[Any]]): String = table match {
+    case Seq() => ""
+    case _ =>
+      val sizes = for (row <- table) yield (for (cell <- row) yield if (cell == null) 0 else cell.toString.length)
+      val colSizes = for (col <- sizes.transpose) yield col.max
+      val rows = for (row <- table) yield formatRow(row, colSizes)
+      formatRows(rowSeparator(colSizes), rows)
+  }
+
+  def formatRows(rowSeparator: String, rows: Seq[String]): String = (
+    rowSeparator ::
+      rows.head ::
+      rowSeparator ::
+      rows.tail.toList :::
+      rowSeparator ::
+      List()).mkString("\n")
+
+  def formatRow(row: Seq[Any], colSizes: Seq[Int]): String = {
+    val cells = (for ((item, size) <- row.zip(colSizes)) yield if (size == 0) "" else ("%" + size + "s").format(item))
+    cells.mkString("|", "|", "|")
+  }
+
+  def rowSeparator(colSizes: Seq[Int]): String = colSizes map { "-" * _ } mkString("+", "+", "+")
+
+  def formatTable(table: Seq[Seq[Any]]): String = {
+    if (table.isEmpty) ""
+    else {
+      // Get column widths based on the maximum cell width in each column (+2 for a one character padding on each side)
+      val colWidths = table.transpose.map(_.map(cell => if (cell == null) 0 else cell.toString.length).max + 2)
+      // Format each row
+      val rows = table.map(_.zip(colWidths).map { case (item, size) => (" %-" + (size - 1) + "s").format(item) }
+        .mkString("|", "|", "|"))
+      // Formatted separator row, used to separate the header and draw table borders
+      val separator = colWidths.map("-" * _).mkString("+", "+", "+")
+      // Put the table together and return
+      (separator +: rows.head +: separator +: rows.tail :+ separator).mkString("\n")
+    }
+  }
+}
